c21bf1b2c4989e62926945a060f3eeec846d0b3e
sambhavw18 <154433854+sambhavw18@users.noreply.github.com>
2024-07-22 23:22:15 +0530

    Update wsgi.py

--- tweetweb/wsgi.py
+++ tweetweb/wsgi.py
@@ -13,5 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tweetweb.settings')
 
-application = get_wsgi_application()
-app=application
+# api/wsgi.py
+app = get_wsgi_application()
